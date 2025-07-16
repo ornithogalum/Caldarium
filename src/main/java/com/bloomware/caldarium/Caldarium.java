ad3f8605e7ae50e3a1f553f43db5e874a87bbe3d
ornithogalum <122026274+ornithogalum@users.noreply.github.com>
2025-07-16 17:52:19 +0200

    add plugin annotation

--- src/main/java/com/bloomware/caldarium/Caldarium.java
+++ src/main/java/com/bloomware/caldarium/Caldarium.java
@@ -1,7 +1,9 @@
 package com.bloomware.caldarium;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.java.annotation.plugin.Plugin;
 
+@Plugin(name = "Caldarium", version = "v1")
 public final class Caldarium extends JavaPlugin {
 
     @Override
