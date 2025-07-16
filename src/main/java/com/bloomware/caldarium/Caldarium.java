0af2b89b0b0e4472db2587fffbf893cf7ef0aebd
ornithogalum <122026274+ornithogalum@users.noreply.github.com>
2025-07-16 18:04:21 +0200

    another try

--- src/main/java/com/bloomware/caldarium/Caldarium.java
+++ src/main/java/com/bloomware/caldarium/Caldarium.java
@@ -2,8 +2,10 @@ package com.bloomware.caldarium;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.java.annotation.plugin.Plugin;
+import org.bukkit.plugin.java.annotation.plugin.author.Author;
 
 @Plugin(name = "Caldarium", version = "v1")
+@Author("starchroot")
 public final class Caldarium extends JavaPlugin {
 
     @Override
